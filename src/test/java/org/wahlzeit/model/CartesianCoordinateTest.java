012d9b7547341bce844347819a1845fd63a7cc75
CY083 <ckoehn@t-online.de>
2022-11-14 01:47:45 +0100

    Changed Test.

--- src/test/java/org/wahlzeit/model/CartesianCoordinateTest.java
+++ src/test/java/org/wahlzeit/model/CartesianCoordinateTest.java
@@ -10,9 +10,11 @@ public class CartesianCoordinateTest {
     public void testDistance() {
 
         CartesianCoordinate testcoord1 = new CartesianCoordinate(1, 1, 1);
-        CartesianCoordinate testcoord2 = new CartesianCoordinate(0, 0, 0);
+        CartesianCoordinate testcoord2 = new CartesianCoordinate(3, 3, 3);
 
-        assertEquals(1.7320508075688772, testcoord1.getDistance(testcoord2), 0);
+        assertEquals(3.4641016151377544, testcoord1.getDistance(testcoord2), 0.0001);
+
+        assertEquals(3.4641016151377544, testcoord1.asSphericCoordinate().getCartesianDistance(testcoord2.asSphericCoordinate()), 0.0001);
 
     }
 
